31c58c70838b82cd8f965df9e48326e34e60bda4
Kyle Trammell <ceotrammell@icloud.com>
2022-05-07 02:37:06 -0500

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# MATHEMA! 
+**(Example Angular Project)**
+
+  
+
+_"Make Math Merry"_
+
+
+[Mathema](https://github.com/angular/angular-cli) Repository
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.1.
+
+  
+
+## Development server
+
+  
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+ 
+
+## Build
+
+  
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+ 
---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import * as mathema from '../../node_modules/mathema/lib/esm/mathema.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mathema-example';
+  arr1 = [1,3,5];
+  arr2 = [2,6,10];
+  arr_in_arr = [1,[2,[3,4]],3,5];
+  obj = [{ val: 1},{ val: 2}]
+
+  ngOnInit() {}
+
+  add() {
+    return mathema.basics.add.simple(-2,1);
+  }
+
+  addTwoArrays() {
+    return mathema.basics.add.twoArrays(this.arr1,this.arr2);
+  }
+
+  addMultiD(arr:any) {
+    return mathema.basics.add.multiD(arr);
+  }
+
+  subtract() {
+    return mathema.basics.subtract.simple(-2,-1);
+  }
+
+  subtractTwoArrays() {
+    return mathema.basics.subtract.twoArrays(this.arr1,this.arr2);
+  }
+
+  subtractMultiD(arr:any) {
+    return mathema.basics.subtract.multiD(arr);
+  }
+
+  multiply() {
+    return mathema.basics.multiply.simple(-2,5);
+  }
+
+  multiplyTwoArrays() {
+    return mathema.basics.multiply.twoArrays(this.arr1,this.arr2);
+  }
+
+  multiplyMultiD(arr:any) {
+    return mathema.basics.multiply.multiD(arr);
+  }
+
+  divide() {
+    return mathema.basics.divide.simple(-2,0.5);
+  }
+
+  divideTwoArrays() {
+    return mathema.basics.divide.twoArrays(this.arr1,this.arr2);
+  }
+
+  divideMultiD(arr:any) {
+    return mathema.basics.divide.multiD(arr);
+  }
+
+}
