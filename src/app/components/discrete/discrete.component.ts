0f77935e054f38927be8ef16e4607c2b4587e1ea
Kyle Trammell <ceotrammell@icloud.com>
2022-07-03 17:39:20 -0500

    add discrete combinations, permutations, set intersections and unions

--- src/app/components/discrete/discrete.component.ts
+++ src/app/components/discrete/discrete.component.ts
@@ -11,12 +11,13 @@ export class DiscreteComponent implements OnInit {
 
   arr1 = [12, 46, 512, 2056];
   arr_in_arr = [12,[2,[46,4]],3,512];
+  arrA = ["a", "b", "c", ["e"]];
+  arrB = ["b", "f", ["g"]];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 
   gcdpair() {
     return mathema.discrete.gcd.pair(80, 44);
@@ -24,6 +25,22 @@ export class DiscreteComponent implements OnInit {
 
   gcdarr(arr:any) {
     return mathema.discrete.gcd.arr(arr);
+  }
+
+  intersection() {
+    return mathema.discrete.sets.intersection(this.arrA, this.arrB);
+  }
+
+  union() {
+    return mathema.discrete.sets.union(this.arrA, this.arrB);
+  }
+
+  ncr() {
+    return mathema.discrete.combination.ncr(10, 4);
+  }
+
+  npr() {
+    return mathema.discrete.permutation.npr(10, 4, true);
   }
 
 }
